b7acb7781845d792251748fe564b8ceff09b79b6
Rafael Smith <rafael.smith@mercadolivre.com>
2023-02-27 19:26:41 -0300

    Projeto Loja De games

---
+++ src/main/java/com/generation/lojadegames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.generation.lojadegames.repository;
+
+import com.generation.lojadegames.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
+}
